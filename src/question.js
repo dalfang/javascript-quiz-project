1912a356e3314e715e7ddd45c932ab70df62cbdc
[Dalin Fangloy] <[d.fangloy@gmail.com]>
2024-05-13 18:38:29 +0200

    Mini project day01

--- src/question.js
+++ src/question.js
@@ -1,7 +1,19 @@
+//*************3.Organize data and functionality using classes******/
+//***day01****/
+
 class Question {
     // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor (text, choices, answer, difficulty){ // 1. constructor (text, choices, answer, difficulty)
+        this.text = text;
+        this.choices =choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
 
-    // 2. shuffleChoices()
+    }
+    shuffleChoices(){ // 2. shuffleChoices()
+        for (let i = this.choices.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+        }
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,46 @@
+//************4.Organize data and functionality using classes******//
 class Quiz {
     // YOUR CODE HERE:
-    //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    
+    }
+     // 2. getQuestion() return the item from the questions array 
+     // at the position of currentQuestionIndex.
+    getQuestion(){ 
+        return this.questions[this.currentQuestionIndex];
+    }
     
     // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex += 1;
+    }
 
     // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) {
+           const j = Math.floor(Math.random() * (i + 1));
+           [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+    // 5. checkAnswer(answer
+    //increase correctAnswers by 1 when called with a correct answer for the current question
+    checkAnswer(answer){
+        if(this.getQuestion().answer === answer){
+            this.correctAnswers += 1;
+        }
+    }
 
     // 6. hasEnded()
+    hasEnded(){
+        return this.currentQuestionIndex >= this.questions.length;
+
+        //return 'true' or 'false' when currentQuestionIndex 
+        //is less than the questions array length
+    }
 }
